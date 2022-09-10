fb96499ded5a8949e37fc13c69c2264f4d5151df
cytocracy <theop.parker@gmail.com>
2022-09-09 20:18:18 -0700

    basic kinematics

--- kinematics.py
+++ kinematics.py
@@ -3,42 +3,26 @@ from vpython import *
 scene.width = scene.height = 600
 scene.title = "Basic Kinematics equation"
 
-g = 9.8
-
-#initialposition
 x0 = 0
-y0 = 200
+y0 = 10
 v0 = 0
 
-
-
-# Create a ball
-ball = sphere(pos=vec(x0, y0, 0), radius=10, color=color.red)
-
-#create floor
-floor = box(pos=vec(0, 0, 0), size=vec(600, 10, 50), color=color.blue)
-
-
+ball = sphere(pos=vec(x0, y0, 0), radius=0.5, color=color.red)
+ball.velocity = vec(0, v0, 0)
+floor = box(pos=vec(0, 0, 0), size=vec(30, 1, 1), color=color.blue)
 
 t=0
-dt = 0.01
-
-
+dt = 0.005
+g = 9.8
 
 while True:
-
-    ball.pos.y -= 1
-    
-    # ball.pos.y = y0 + v0 + 0.5 * -1* g * t**2
-
-    # rate(100)
-
-    # x0 = ball.pos.x
-    # y0 = ball.pos.y
-
-    # v0 = v0 + g * t
-    # # print("Running")
-    # t += dt    
+    rate(400)
+    if ball.pos.y < floor.pos.y + ball.radius*2:
+        ball.pos.y = floor.pos.y + ball.radius*2
+        ball.velocity.y = -ball.velocity.y * 0.9
 
 
+    ball.velocity.y = ball.velocity.y - g*dt
+    ball.pos = ball.pos + ball.velocity*dt
+    t+=dt
 
